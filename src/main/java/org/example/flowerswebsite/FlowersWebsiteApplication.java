62d70de4239fdbc325c2cb771663d55d46d8370c
tanyyaako <tanyyaako@icloud.com>
2024-10-25 23:22:02 +0300

    initial commit

---
+++ src/main/java/org/example/flowerswebsite/FlowersWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.flowerswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/flowerswebsite/FlowersWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flowerswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
